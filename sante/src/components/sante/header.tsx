7a276f874b06d05bd65daf969cd7faf0368e04c5
Ruan Silva <ruanrafaelleite@gmail.com>
2023-11-28 19:23:28 -0300

    added header

--- sante/src/components/sante/header.tsx
+++ sante/src/components/sante/header.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export const Header = () => {
   return (
-    <header className="bg-white shadow-xl">
+    <header className="bg-white shadow-2xl">
       <nav
         className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8"
         aria-label="Global"
